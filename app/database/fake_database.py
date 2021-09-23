131ceb93f0ac4a130ba50b6ad2a4954e5c3440cc
Andrey Shein <andrey.shein.spb@gmail.com>
2021-09-24 00:02:17 +0300

    add fake database

--- app/database/fake_database.py
+++ app/database/fake_database.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from app.database.base import Base
 
 from app.requests.notes_getting import NotesGettingRequest
@@ -5,19 +7,39 @@ from app.requests.note_creation import NoteCreationRequest
 from app.requests.note_deleting import NoteDeletingRequest
 from app.requests.note_editing import NoteEditingRequest
 
+from app.responses.notes_getting import NoteDescription
 from app.responses.notes_getting import NotesGettingResponse
 
 
 class FakeDatabase(Base):
+    notes = []
+    next_note_id = len(notes)
 
     def get_notes(self, request: NotesGettingRequest) -> NotesGettingResponse:
-        raise NotImplementedError
+        result: List[NoteDescription] = []
+        for note in self.notes:
+            if note["author_id"] == request.author_id:
+                result.append(NoteDescription(note["author_id"], note["topic"]))
+        return NotesGettingResponse(result)
 
     def create_note(self, request: NoteCreationRequest) -> None:
-        raise NotImplementedError
+        self.notes.append({
+            "note_id": self.next_note_id,
+            "author_id": request.author_id,
+            "topic": request.topic,
+            "content": request.content
+        })
+        self.next_note_id += 1
 
     def delete_note(self, request: NoteDeletingRequest) -> None:
-        raise NotImplementedError
+        for i in range(len(self.notes)):
+            if self.notes[i]["note_id"] == request.note_id:
+                self.notes.pop(i)
+                return
 
     def edit_note(self, request: NoteEditingRequest) -> None:
-        raise NotImplementedError
+        for note in self.notes:
+            if note["note_id"] == request.note_id:
+                note["topic"] = request.new_topic
+                note["content"] = request.new_content
+                return
