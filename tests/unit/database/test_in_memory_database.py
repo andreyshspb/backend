a7d911cf01274fb1e9caebad10fda4394c65747a
Andrey Shein <andrey.shein.spb@gmail.com>
2021-09-25 20:26:33 +0300

    add other tests

--- tests/unit/database/test_in_memory_database.py
+++ tests/unit/database/test_in_memory_database.py
@@ -29,7 +29,7 @@ def test_create_one_note():
     assert database.notes[0]["content"] == "Watch the last lecture"
 
 
-def test_create_two_note():
+def test_create_two_note_with_different_authors():
     database = InMemoryDatabase()
 
     request = NoteCreationRequest(
@@ -55,6 +55,34 @@ def test_create_two_note():
 
     assert database.notes[1]["note_id"] == 1
     assert database.notes[1]["author_id"] == 2
+    assert database.notes[1]["topic"] == "HSE"
+    assert database.notes[1]["content"] == "It is my university"
+
+
+def test_create_two_note_with_the_same_author():
+    database = InMemoryDatabase()
+
+    database.create_note(NoteCreationRequest(
+        author_id=1,
+        topic="Math statistics",
+        content="Do nothing"
+    ))
+    database.create_note(NoteCreationRequest(
+        author_id=1,
+        topic="HSE",
+        content="It is my university"
+    ))
+
+    assert len(database.notes) == 2
+    assert database.next_note_id == 2
+
+    assert database.notes[0]["note_id"] == 0
+    assert database.notes[0]["author_id"] == 1
+    assert database.notes[0]["topic"] == "Math statistics"
+    assert database.notes[0]["content"] == "Do nothing"
+
+    assert database.notes[1]["note_id"] == 1
+    assert database.notes[1]["author_id"] == 1
     assert database.notes[1]["topic"] == "HSE"
     assert database.notes[1]["content"] == "It is my university"
 
@@ -142,7 +170,7 @@ def test_get_notes_with_count():
 
 # delete_note() method
 
-def test_delete_note():
+def test_delete_one_note():
     database = InMemoryDatabase()
 
     first = NoteCreationRequest(
@@ -169,6 +197,44 @@ def test_delete_note():
     assert database.notes[0]["author_id"] == 2
     assert database.notes[0]["topic"] == "Second"
     assert database.notes[0]["content"] == "Hello world"
+
+
+def test_double_delete():
+    database = InMemoryDatabase()
+
+    database.create_note(NoteCreationRequest(
+        author_id=1,
+        topic="Statistics",
+        content="Watch seminar"
+    ))
+
+    database.delete_note(NoteDeletingRequest(
+        note_id=0
+    ))
+
+    try:
+        database.delete_note(NoteDeletingRequest(
+            note_id=0
+        ))
+    except Exception as exception:
+        pytest.fail("Unexpected error:\n" + str(exception))
+
+    assert len(database.notes) == 0
+    assert database.next_note_id == 1
+
+
+def test_delete_from_empty_database():
+    database = InMemoryDatabase()
+
+    try:
+        database.delete_note(NoteDeletingRequest(
+            note_id=0
+        ))
+    except Exception as exception:
+        pytest.fail("Unexpected error:\n" + str(exception))
+
+    assert len(database.notes) == 0
+    assert database.next_note_id == 0
 
 
 # edit_note() method
@@ -208,3 +274,47 @@ def test_edit_note():
     assert database.notes[1]["author_id"] == 1
     assert database.notes[1]["topic"] == "Third"
     assert database.notes[1]["content"] == "What???"
+
+
+def test_edit_in_empty_database():
+    database = InMemoryDatabase()
+
+    try:
+        database.edit_note(NoteEditingRequest(
+            note_id=0,
+            new_topic="HSE",
+            new_content="It is my university"
+        ))
+    except Exception as exception:
+        pytest.fail("Unexpected error:\n" + str(exception))
+
+    assert len(database.notes) == 0
+    assert database.next_note_id == 0
+
+
+def test_double_editing_note():
+    database = InMemoryDatabase()
+    database.create_note(NoteCreationRequest(
+        author_id=1,
+        topic="HSE",
+        content="It is my university"
+    ))
+
+    database.edit_note(NoteEditingRequest(
+        note_id=0,
+        new_topic="Python",
+        new_content="Do Homework"
+    ))
+    database.edit_note(NoteEditingRequest(
+        note_id=0,
+        new_topic="ML",
+        new_content="Watch the last lecture"
+    ))
+
+    assert len(database.notes) == 1
+    assert database.next_note_id == 1
+
+    assert database.notes[0]["note_id"] == 0
+    assert database.notes[0]["author_id"] == 1
+    assert database.notes[0]["topic"] == "ML"
+    assert database.notes[0]["content"] == "Watch the last lecture"
